9d8323fd2931eed65f352bc92e27eaceac923320
christian.riese <christian.riese@dieboldnixdorf.com>
2019-05-23 13:30:11 +0200

    hopefully line break is fixed...

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -22,7 +22,7 @@ import {Form, Formik} from "formik";
 import SearchIcon from '@material-ui/icons/Search';
 
 function TopBar(props) {
-    const {drawerOpen, toogleDrawer, us, history, session, tablet, tableLandscape, mobile, mobileLandscape} = props;
+    const {drawerOpen, toogleDrawer, us, history, session, mobile, mobileLandscape} = props;
 
     let isIE = /*@cc_on!@*/false || !!document.documentMode;
 
@@ -39,7 +39,7 @@ function TopBar(props) {
                                         query={search}
                                         liveSearch
                                         name="pattern"
-                                        placeholder={mobile || mobileLandscape ? " " : "Suchen..."}
+                                        placeholder={mobile || mobileLandscape || tablet || tableLandscape ? " " : "Suchen..."}
                                         variant="outlined"
                                         variables={{pattern: values.pattern, us: us}}
                                         onChange={(node, live) => {
